980dfabeedef85009356a43323726c413faecc72
MiltonMartinez29 <alfredoxd321@gmail.com>
2024-04-20 21:59:27 -0700

    Boton_de_like

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,19 @@ class MyAppState extends ChangeNotifier {
   void getNext() {
     current = WordPair.random();
     notifyListeners();
+    
   }
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
 }
 
 class MyHomePage extends StatelessWidget {
@@ -38,6 +50,13 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+
+       IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
     return Scaffold(
       body: Center(
         child: Column(
@@ -45,13 +64,25 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
+            Row(
+              mainAxisSize: MainAxisSize.min, 
+              children: [
+               ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
 
-                print('button pressed!');
-              },
-              child: Text('Next'),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                    ),
+              ],
             ),
           ],
         ),
@@ -59,6 +90,7 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
 
 class BigCard extends StatelessWidget {
   const BigCard({
